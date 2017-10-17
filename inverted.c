a0e4b64f1389883c1191e9347f138613ca487c29
Jolville <jesse.colville@outlook.com>
2017-10-17 11:09:44 +1100

    inverted serena canges

--- inverted.c
+++ inverted.c
@@ -1,11 +1,12 @@
-// Gets an index of words with each url that contains the word 
-// Puts this in a linked list of words, where each node has a linked list of URLS 
-// Outputs it into a txt file, with words sorted in alphabetical order and URLS sorted in ascending order 
-// Written by Jesse Colville and Serena Xu 
+// Gets an index of words with each url that contains the word
+// Puts this in a linked list of words, where each node has a linked list of URLS
+// Outputs it into a txt file, with words sorted in alphabetical order and URLS sorted in ascending order
+// Written by Jesse Colville and Serena Xu
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h> //for using tolower
 #include "set.h"
 #include "graph.h"
 #include "queue.h"
@@ -42,10 +43,10 @@ void showlListRep(lListRep *);
 
 int main(void) {
 
-	FILE *fp; 
-	char *urlIn = {0};
-	char *urlInTxt = {0};
-	char *word = {0}; 
+	FILE *fp;
+	char *urlIn = malloc(sizeof(char)*100);
+	char *urlInTxt = malloc(sizeof(char)*100);
+	char *word = malloc(sizeof(char)*100);
 
 	// Get set of urls
 	Set urls = getCollection("collection.txt");
@@ -53,42 +54,84 @@ int main(void) {
 	// Create list rep
 	lListRep *L = createLList();
 
-	addWord(L, "word 1");
+	/*addWord(L, "worda");
 	if (L->first == NULL) return 0;
-	addWord(L, "word 1");
-	addWord(L, "word 2");
-	addURL(L, "word 1", "url11");
-	addURL(L, "word 1", "url11");
-	addURL(L, "word 1", "url12");
-	showlListRep(L);
-	
-	// For each URL in set urls, make list of URLS for each word 
-	for (int i=0; i<nElems(urls); i++) {
+	addWord(L, "worda");
+	addWord(L, "wordb");
+	addURL(L, "worda", "url1");
+	addURL(L, "worda", "url2");
+	printf("0\n");
+	addURL(L, "wordb", "url1");
+	showlListRep(L);*/
 
-		// Open URL txt file 	
+	// For each URL in set urls, make list of URLS for each word
+	for (int i=0; i<nElems(urls); i++) {
+		//printf("4\n");
+		// Open URL txt file
+		//printf("%s\n", getNth(urls, i));
 		strcpy(urlIn, getNth(urls, i));
+		//printf("%s\n", urlIn);
+		//printf("3\n");
 		strcpy(urlInTxt, getNth(urls, i));
 		strcat(urlInTxt, ".txt");
 		fp = fopen(urlInTxt, "r");
+		//printf("%s\n", urlInTxt);
 		if (fp == NULL) {
 			printf("Couldn't open file.\n");
 			exit(1);
 		}
 		// Scan for words
-		fscanf(fp, "%s", word); 
-		if (strcmp(word, "Section-2") == 1) {
-			// For each word 
-			while (strcmp(word, "#end") != 0) {
-				// Remove spaces before/after word 
-				// Convert all characters to lower case 
-				// Remove punctuation marks (dot, comma, semicolon, question mark) 
-				// Put each word into list 
-				// Put URL with each word if not already there 
-			} 
-		} 
-	} 
-
-	// Output to txt file in alphabetical order 
+		fscanf(fp, "%1023s", word);
+		while (strcmp(word, "#end") != 0) {
+			if (strcmp(word, "Section-2") == 1) {
+				while (strcmp(word, "#end") != 0) {
+			printf("hi\n");
+			fscanf(fp, "%1023s", word);
+			printf("%s\n", word);
+			// If start of words section, enter the loop
+			if (strcmp(word, "Section-2") == 1) {
+				printf("2\n");
+				while (strcmp(word, "#end") != 0) {
+					fscanf(fp, "%s", word);
+					// For each word, convert to lower case, remove spaces & punctuation
+					for (int i=0; word[i]!= '\0'; i++) {
+						if (word[i] == ' ' || word[i] == '.' || word[i] == ',' || word[i] == ';' || word[i] == '?' ) {
+							word[i] = '\0';
+						}
+						word[i] = tolower(word[i]);
+					}
+					// If word not in lListRep
+					if (findWord(L, word) == 0) {
+						// Put word into list
+						addWord(L, word);
+					} else {
+						// Put URL with each word if not already there
+						if (findURL(L->first, word) == 0 ) {
+							addURL(L, word, urlIn);
+						}
+					}
+				}
+			}
+		}
+			}
+		}
+	}
+	// Prints to output file.
+	FILE *outputStream;
+	outputStream = fopen("invertedIndex.txt", "w");
+	if (outputStream == NULL) {
+		fprintf(stderr, "txt file creation failed\n");
+	}
+	int j = 0;
+	for (key *k = L->first; k != NULL; k = k->next) {
+		j = 1;
+		fprintf(outputStream, "%s ", k->word);
+		for (urlNode *n = k->urls; n != NULL; n = n->next) {
+			fprintf(outputStream, " %s", n->url);
+		}
+		printf("\n");
+	}
+	if (j == 0) printf("Empty list.\n");
 	return 0;
 }
 
@@ -107,61 +150,67 @@ lListRep *createLList() {
 
 // Add a word to the list (treat as a set)
 void addWord(lListRep *L, char *w) {
+	//printf("%s", w);
 	assert(L);
 	if (findWord(L, w)) return;
 
-	// makes a new pointer to a word 
+	// makes a new pointer to a word
 	key *new = malloc(sizeof(key));
 
-	// makes a new array for the word in the newly made key 
-	new->word = malloc(strlen(w+1));
+	// makes a new array for the word in the newly made key
+	//printf("length of word is %lu\n", strlen(w));
+	new->word = malloc(strlen(w)+1);
 
-	// tests that key and word creation succeeded 
+	// tests that key and word creation succeeded
 	if (new == NULL || new->word == NULL) {
 		fprintf(stderr, "Malloc failed: Couldn't create key.\n");
 		exit(1);
 	}
 
-	// increases the number of words in set by 1 
+	// increases the number of words in set by 1
 	L->nWords++;
 
-	// puts the new word into the array 
+	printf("before: %s\n", w);
+	// puts the new word into the array
 	strcpy(new->word, w);
+	printf("after: %s\n", new->word);
 
-	// makes the word's url list empty 
+	// makes the word's url list empty
 	new->urls = NULL;
 
-	// makes the word's next pointer point to NULL  
+	// makes the word's next pointer point to NULL
 	new->next = NULL;
 
-	// if the set is empty make the word the first word and return 
+	// if the set is empty make the word the first word and return
 	if (L->first == NULL) {
 		L->first = new;
 		return;
 	}
 
-	// if the word is not the first word 
-	// make a pointer, curr, point to the first word 
+	// if the word is not the first word
+	// make a pointer, curr, point to the first word
 	key *curr = L->first;
-	
-	// iterate through list and insert word in alphabetical order 
-	for (curr = L->first; curr->next != NULL; curr = curr->next) { 
-		// compare each character in both the word in the list and the word to be inserted 
-		int k=0; 
-		while (k<strlen(w)) {
-			if (new->word[k] == curr->word[k]) { 
-				k++; 
-			} else if (new->word[k] > curr->word[k]) {
-				if (new->word[k] < curr->next->word[0]) {
-					new->next = curr->next; 
-					curr->next = new; 
-					return; 
-				} 
-			}
-		} 
-	} if (new->word[0] > curr->word[0]) {
-		curr->next = new; 
-	} 
+	key *prev = L->first;
+
+	// iterate through list and insert word in alphabetical order
+	while (curr != NULL) {
+		//printf("entered 1\n");
+		// compare each character in both the word in the list and the word to be inserted
+		if (strcmp(new->word, curr->word) > 0) { //new word is bigger than word in list
+			prev = curr;
+			curr = curr->next;
+		} else {
+			//printf("inside else\n");
+			// insert before current word
+			new->next = curr;
+			curr = new;
+			return;
+		}
+		//printf("success\n");
+	}
+	//printf("hello\n");
+	prev->next = new;
+	//printf("hi\n");
 	return;
 }
 
@@ -187,7 +236,7 @@ void addURL(lListRep *L, char *word, char *url) {
 		}
 		if (strcmp(curr->word, word) == 0) {
 			break;
-		} else { 
+		} else {
 			curr = curr->next;
 		}
 	}
